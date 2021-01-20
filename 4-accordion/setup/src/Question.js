6d8dbc368098e5421514f267ebca526d4a24f468
hmhuy97 <39719651+hmhuy97@users.noreply.github.com>
2021-01-20 17:00:06 +0700

    up

--- 4-accordion/setup/src/Question.js
+++ 4-accordion/setup/src/Question.js
@@ -1,7 +1,12 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = (tit) => {
+  return <article className="question">
+    <header>
+      <h4>{title}</h4>
+    </header>
+  </article>;
 };
+
 
 export default Question;
