8d23a42788510d86c4b31b1db2f6c4f6b01c2e20
hmhuy97 <39719651+hmhuy97@users.noreply.github.com>
2021-02-24 21:01:30 +0700

    331

--- 9-color-generator/setup/src/App.js
+++ 9-color-generator/setup/src/App.js
@@ -5,11 +5,18 @@ import Values from "values.js";
 
 function App() {
   const [color, setColor] = useState("");
-  const [error, SetError] = useState(false);
+  const [error, setError] = useState(false);
   const [list, setList] = useState([]);
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("hello");
+    try {
+      let colors = new Values(color).all(10);
+      setList(colors);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
   };
   return (
     <>
@@ -21,6 +28,7 @@ function App() {
             value={color}
             onChange={(e) => setColor(e.target.value)}
             placeholder="#F15025"
+            className={`${error ? "error" : null}`}
           />
           <button className="btn" type="submit">
             submit
@@ -29,7 +37,10 @@ function App() {
       </section>
 
       <section className="colors">
-        <h4>List goes here</h4>
+        {list.map((color, index) => {
+          console.log(color);
+          return <SingleColor key={index} {...color} index={index} />;
+        })}
       </section>
     </>
   );
