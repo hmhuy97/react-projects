76f1f0e6e6b4fa430c793b351d524f053aa0bf3f
hmhuy97 <39719651+hmhuy97@users.noreply.github.com>
2021-03-08 17:05:00 +0700

    53320

--- 12-sidebar-modal/setup/src/Home.js
+++ 12-sidebar-modal/setup/src/Home.js
@@ -1,8 +1,15 @@
 import React, { useContext } from 'react'
 import { FaBars } from 'react-icons/fa'
+import {AppContext} from './context'
 
 const Home = () => {
-  return <h2>home component</h2>
+  const data = useContext(AppContext)
+  return <main>
+    <button className="sidebar-toggle">
+      <FaBars/>
+    </button>
+    <button className="btn">Show modal</button>
+  </main>
 }
 
 export default Home
