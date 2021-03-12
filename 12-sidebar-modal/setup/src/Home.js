67f26fa598728cf75c9daf68af194862258e27e0
hmhuy97 <39719651+hmhuy97@users.noreply.github.com>
2021-03-12 16:36:20 +0700

    Update Home.js

--- 12-sidebar-modal/setup/src/Home.js
+++ 12-sidebar-modal/setup/src/Home.js
@@ -1,15 +1,18 @@
-import React, { useContext } from 'react'
-import { FaBars } from 'react-icons/fa'
-import {AppContext} from './context'
+import React, { useContext } from "react";
+import { FaBars } from "react-icons/fa";
+import { AppContext } from "./context";
 
 const Home = () => {
-  const data = useContext(AppContext)
-  return <main>
-    <button className="sidebar-toggle">
-      <FaBars/>
-    </button>
-    <button className="btn">Show modal</button>
-  </main>
-}
+  const data = useContext(AppContext);
+  console.log("7", data);
+  return (
+    <main>
+      <button className="sidebar-toggle">
+        <FaBars />
+      </button>
+      <button className="btn">Show modal</button>
+    </main>
+  );
+};
 
-export default Home
+export default Home;
